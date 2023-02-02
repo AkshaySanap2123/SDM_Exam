5077d25184dededf88f4e59d39c22eff926b4364
Akshay <Akshaysanap2123@gmail.com>
2023-02-02 16:00:13 +0530

    all

--- Fibo.java
+++ Fibo.java
@@ -1,7 +1,7 @@
 class Fibo
 {
     public static void main(String[] args) {
-        int a=0, b=1, c,i, count=20;
+        int a=0, b=1, c,i, count=15;
         System.out.println(a+" "+b);
 
         for( i=0; i<count; i++)
